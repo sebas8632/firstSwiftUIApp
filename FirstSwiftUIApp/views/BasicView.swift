9bd6ce2248f42d35d45606782a7241fb6dbc395f
Juan Sebastián Flórez Saavedra <sebastian8632@gmail.com>
2020-07-07 23:14:57 -0500

    first commit

---
+++ FirstSwiftUIApp/views/BasicView.swift
@@ -0,0 +1,9 @@
+//
+//  BasicView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Juan Sebastian Florez Saavedra on 7/07/20.
+//  Copyright © 2020 Juan Sebastian Florez Saavedra. All rights reserved.
+//
+
+import Foundation
---
+++ FirstSwiftUIApp/views/ListView.swift
@@ -0,0 +1,9 @@
+//
+//  ListView.swift
+//  FirstSwiftUIApp
+//
+//  Created by Juan Sebastian Florez Saavedra on 7/07/20.
+//  Copyright © 2020 Juan Sebastian Florez Saavedra. All rights reserved.
+//
+
+import Foundation
